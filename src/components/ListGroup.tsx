15dda252df6700d52948e3d05bd5c92d9c9456d0
Paul Martinez <paulmartinez.career@gmail.com>
2024-12-21 01:52:14 -0800

    Add list conditional rendering.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,6 +4,7 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items in the list</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
