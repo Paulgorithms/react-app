457869f42fa6207c2201b7c29466ce84527f40eb
Paul Martinez <paulmartinez.career@gmail.com>
2025-02-04 09:32:24 -0800

    Build a form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores
-        voluptates eius debitis tempore dolorem quo, mollitia obcaecati, esse
-        nesciunt nulla soluta molestias, labore possimus reiciendis porro
-        dolores qui est ducimus id officia optio? Sapiente officia voluptates id
-        debitis, ab, corrupti et placeat eveniet tempore, aperiam fugiat nisi
-        maiores? Consequatur labore quisquam qui quam delectus molestiae
-        corrupti, quos sequi voluptates optio facere necessitatibus! Non quia
-        aperiam aspernatur nostrum voluptas sit minus rerum excepturi labore!
-        Vero voluptatum facere libero blanditiis, nostrum accusamus est porro
-        illum corporis ipsum provident ab aut assumenda corrupti quis aliquid
-        dolorem. Sapiente tenetur similique adipisci quibusdam ipsa hic.
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
